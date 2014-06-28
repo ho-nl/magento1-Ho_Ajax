4246236c29372f20a44e9fd7c588d0c8ebff0375
Paul Hachmang <paul@h-o.nl>
2014-06-28 16:55:47 +0200

    [Fix] License information set to OSL

--- app/code/local/Ho/BootstrapAjaxCart/Helper/Data.php
+++ app/code/local/Ho/BootstrapAjaxCart/Helper/Data.php
@@ -4,21 +4,19 @@
  *
  * NOTICE OF LICENSE
  *
- * This source file is subject to the H&O Commercial License
- * that is bundled with this package in the file LICENSE_HO.txt.
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
  * It is also available through the world-wide-web at this URL:
- * http://www.h-o.nl/license
+ * http://opensource.org/licenses/osl-3.0.php
  * If you did not receive a copy of the license and are unable to
  * obtain it through the world-wide-web, please send an email
- * to info@h-o.com so we can send you a copy immediately.
+ * to license@magentocommerce.com so we can send you a copy immediately.
  *
  * @category    Ho
  * @package     Ho_BootstrapAjaxCart
- * @copyright   Copyright © 2013 H&O (http://www.h-o.nl/)
- * @license     H&O Commercial License (http://www.h-o.nl/license)
+ * @copyright   Copyright © 2014 H&O (http://www.h-o.nl/)
+ * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  * @author      Paul Hachmang – H&O <info@h-o.nl>
- *
- * 
  */
  
 class Ho_BootstrapAjaxCart_Helper_Data extends Mage_Core_Helper_Abstract
